c1db8550273c139baffa80083a5e5194b6bb1da9
hzhou17 <hr.zhou9446@gmail.com>
2020-02-07 04:31:39 -0500

    Update sketch.js

--- clock/empty-example/sketch.js
+++ clock/empty-example/sketch.js
@@ -1,99 +1,102 @@
 var raise;
 
+function remap(start, Value, end, newStart, newEnd)
+{
+  return  (newStart + (newEnd - newStart) * (Value - start)/(end - start))
+}
 
+function color_gradient(width, height, thickness) // height and width of the canvas. 
+{                           //thickness of a subdivision. The thinner, the smoother.
+  for (i = 0; i < height; i+= thickness)
+  {
+      var new_color = remap(0, i, height, 0, 1);
 
+      push();
 
-function I(tx, ty)
+        fill(new_color);
+        noStroke()
+
+        rect(0, i, width, thickness);
+
+      pop();
+  }
+}
+
+function I() // tree trunk
+{
+    push()
+      strokeWeight(2)
+      rect(0, height/2, height/20/15, height/20)
+    pop()
+}
+
+function tree(tx, ty)
 {
     push()
 
-      strokeWeight(2)
-      rect(width/2, height/2, height/20/15, height/20)
+      translate(tx, ty)
+        I()
+
+      translate(0, -height/20)
+        quad(- 10, midH - 10, 
+             + 10, midH - 10, 
+             + 20, midH + 17.5, 
+             - 20, midH + 17.5)
+
+      translate(0, -20)
+        quad(- 7.5, midH - 7.5, 
+             + 7.5, midH - 7.5, 
+             + 15, midH + 10, 
+             - 15, midH + 10)
+
+      translate(0, -7.5)
+        triangle(0, height/2 - 20, 0 -12 , height/2, 0 + 12, height/2)
 
     pop()
 }
 
 
-  function tree(tx, ty)
-  {
-      push()
-
-        translate(tx, ty)
-
-        I()
-
-        translate(0, -height/20)
-
-        quad(midW - 10, midH - 10, 
-             midW + 10, midH - 10, 
-             midW + 20, midH + 17.5, 
-             midW - 20, midH + 17.5)
-
-        translate(0, -20)
-
-        quad(midW - 7.5, midH - 7.5, 
-             midW + 7.5, midH - 7.5, 
-             midW + 15, midH + 10, 
-             midW - 15, midH + 10)
-
-        translate(0, -7.5)
-
-        triangle(width/2, height/2 - 20, width/2 -12 , height/2, width/2 + 12, height/2)
-
-      pop()
-
-  }
 
 
-  function remap(start, Value, end, newStart, newEnd)
-  {
-    return  (newStart + (newEnd - newStart) * (Value - start)/(end - start))
-  }
+function mountain_range(width, height, scaleX, tx, ty)
+{
+  push();
 
+    translate(tx, ty);
 
+      push();
+        scale(scaleX,1);
+          beginShape();
+              for (i = -width; i < width; i+=1)
+              {
+                // noFill();
+                raise = sin(i* .1840) + 
+                        sin(i* .1900) + 
+                        sin(i* .1960) +  
+                        sin(i)* .2020 //magic noise number
+         
+                raise *= 30 ; 
 
+                vertex(i, height/2 + raise);
+              }
+          endShape(CLOSE);
+      pop();
 
-  function color_gradient(width, height, thickness) // height and width of the canvas. 
-  {                           //thickness of a subdivision. The thinner, the smoother.
-    for (i = 0; i < height; i+= thickness)
+    for (i = -width; i < width; i+=5)
     {
-        var new_color = remap(0, i, height, 0, 1);
-
-        push();
-
-          fill(new_color);
-          noStroke()
-
-          rect(0, i, width, thickness);
-
-        pop();
-    }
-  }
-
-  function mountain_range(width, height)
-  {
-    push()
-    // scale(20,1)
-    beginShape()
-        for (i = 0; i < width; i+=1)
-        {
-          noFill();
-
-          raise = sin(i * .1960) +  sin(i) * .2020 //magic noise number
+              raise = sin(i* .1840) + 
+                      sin(i* .1900) + 
+                      sin(i* .1960) +  
+                      sin(i)* .2020 //magic noise number
    
           raise *= 30 ; 
 
-          vertex(i*20, height/2 + raise)
+          tree(i*scaleX, raise - height/20);
+    }
 
-          // tree(i*20, height/2 + raise)
-          rect(i*20, height/2 + raise, 5, 5)
+  pop();
 
-        }
-    endShape()
-    pop()
-
-
-  }
+}
 
 
 
@@ -106,7 +109,7 @@ function I(tx, ty)
 function setup()
 {
   createCanvas(640, 360);
-  midW = width/2
+
   midH = height/2
 
   colorMode(RGB, 1);
@@ -128,11 +131,13 @@ function draw()
     color_gradient(width, height, 30)
 
 
-    mountain_range(width, height)
 
+      fill(0.8)
+        mountain_range(width, height, 30, 350, -20) 
 
+            mountain_range(width, height, 50, 0, 100)
 
-
+    // tree(0,0)
 
 
 
