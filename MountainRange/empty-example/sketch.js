3e1a655e1de7021c7730f9e2dbd0a45a170d98b1
hzhou17 <hr.zhou9446@gmail.com>
2020-02-14 04:09:51 -0500

    fixedScale

--- MountainRange/empty-example/sketch.js
+++ MountainRange/empty-example/sketch.js
@@ -2,6 +2,11 @@ var raise;
 var tree_sx;
 var tree_sy;
 
+var trunk_len = 30
+
+
+// var  = 0;
+
 function remap(start, Value, end, newStart, newEnd)
 {
     return  (newStart + (newEnd - newStart) * (Value - start)/(end - start))
@@ -42,7 +47,7 @@ function star(len, wid) // double for loop with noise in x and y translation to 
 
                 translate(width * noise(i, j), height * noise(i, j))
 
-                circle(0, 0, noise(i)*3)
+                circle(0, 0, noise(i)*3.5)
 
             pop();
          }
@@ -52,48 +57,37 @@ function star(len, wid) // double for loop with noise in x and y translation to 
 
 
 
-function I() // tree trunk
-{
-    push()
-      strokeWeight(2)
-      rect(0, height/2, height/20/15, height/25)
-    pop()
-}
-
 function tree(tx, ty, tree_sx, tree_sy)
 {
     push()
 
+        translate(0, height/2) //height/2 because mountain ranges also start there
         translate(tx, ty)
         scale(tree_sx, tree_sy)
 
         strokeWeight(2)
-        rect(0, height/2, height/20/15, height/25)
+        rect(0, 0, 2.5, trunk_len) //tree trunk
 
+        quad(- 10,   - 9, 
+             + 10,   - 7, 
+             + 18,   + 12, 
+             - 18,   + 12)
 
-        translate(0, -height/40)
-        quad(- 10, midH - 9, 
-             + 10, midH - 7, 
-             + 20, midH + 17.5, 
-             - 20, midH + 17.5)
+        quad(- 7.5,   -20 - 5, 
+             + 7.5,   -20 - 6, 
+             + 15,    -20 + 10, 
+             - 15,    -20 + 10)
 
-        translate(0, -20)
-        quad(- 7.5, midH - 5, 
-             + 7.5, midH - 6, 
-             + 15, midH + 10, 
-             - 15, midH + 10)
-
-        translate(0, -7.5)
-        triangle(0, height/2 - 20, 
-                0 -12 , height/2, 
-                0 + 12, height/2+2)
+        triangle(0,  - 20- 7.5 - 20, 
+                0 -12 ,  - 20 - 8, 
+                0 + 12,  - 20 - 8 + 2)
 
     pop()
 }
 
 
 
-function mountain_range(scaleX, tx, ty, background_color, draw_tree)
+function mountain_range(scaleX, tx, ty, background_color, draw_tree, layer_scale)
 {
   push();
 
@@ -102,7 +96,7 @@ function mountain_range(scaleX, tx, ty, background_color, draw_tree)
       push();
         scale(scaleX,1); //scale horizontally to reduce mountain range function frequency, 
                             //so that repeating patterns won't be noticed
-              for (i = -width; i < width; i+=0.1)
+              for (i = -width/2; i < width; i+=0.1)
               {
                 stroke(background_color);
 
@@ -118,11 +112,12 @@ function mountain_range(scaleX, tx, ty, background_color, draw_tree)
       pop();
 
     if (draw_tree != 0) //use a number argument       
-      for (i = -width; i < width; i+= random(3,10))
+    {  for (i = -width/2; i < width; i+= 1)
       {
-            tree_sx=random(0.75, 1.25)
-            tree_sy=random(0.5, 1.5)
-
+            tree_sx = random(0.5, 1.5) * layer_scale
+            tree_sy = random(1, 1.5)  * layer_scale
+            // tree_sx = 1
+            // tree_sy = 1
                 raise = sin(i* .1840) + 
                         sin(i* .1900) + 
                         sin(i* .1960) +  
@@ -130,9 +125,9 @@ function mountain_range(scaleX, tx, ty, background_color, draw_tree)
      
             raise *= 30 ; 
 
-            tree(i*scaleX, raise - height/25, tree_sx, tree_sy); //match the tree translate x with the mountain range scale x. 
+            tree(i * scaleX,  raise - trunk_len * tree_sy - 2, tree_sx, tree_sy); //match the tree translate x with the mountain range scale x. 
       }
-
+    }  
   pop();
 
 }
@@ -144,7 +139,7 @@ function mountain_range(scaleX, tx, ty, background_color, draw_tree)
 function setup()
 {
     createCanvas(1280, 720);
-    midH = height/2
+     // = height/2
 
     colorMode(RGB, 1);
 
@@ -156,16 +151,17 @@ function setup()
     star(1280, 400)
 
 
+    fill(0.2, 0.5, 0.2)
 
     mountain_range(100, 250, 30, 0.8, 0)
 
-    mountain_range(100, 50, 20, 0.5, 0)
+    mountain_range(100, 50, 20, 0.5, 1, 0.6)
 
-    mountain_range(100, 350, 80, 0.4, 0) //scaleX, tx, ty, color, alpha, draw_tree
+    mountain_range(100, 350, 80, 0.4, 1, 0.7) //scaleX, tx, ty, color, alpha, draw_tree
 
-    mountain_range(50, 350, 150, 0.2, 1) 
+    mountain_range(50, 350, 150, 0.2, 1 , 0.8)
 
-    mountain_range(30, -500, 300, 0.1, 1)
+    mountain_range(30, -500, 300, 0.1, 1, 1)
 
 }
 
