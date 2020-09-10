295dfeaa24b4b3cc120af7248f39eb295353211c
hzhou17 <44785490+hzhou17@users.noreply.github.com>
2020-09-09 23:27:46 -0400

    scale

---
+++ Tilings/Half Circle/8-sided.js
@@ -0,0 +1,95 @@
+var bottomHeight //the height from center to bottom, which is shorter than radius
+var sideLength
+
+var polygonRadius = 35
+
+var sides = 8
+
+
+function setup()
+{
+    createCanvas(600, 600);
+    colorMode(RGB, 1)
+
+    background(0)
+
+
+	fourHexgons(polygonRadius, sides)
+
+
+	for (var i = 0; i < width/(polygonRadius*3)+1; i++)
+	{
+		for (var j = 0; j <= height/(5*bottomHeight)+2; j++)
+		{
+			print(i, j)
+
+			push()
+
+				translate( i*(2 * polygonRadius + sideLength), j * (4*bottomHeight))
+
+				fourHexgons(polygonRadius, sides)
+
+			pop()
+		}
+	}
+
+    //saveCanvas('myCanvas', 'png');
+}
+
+
+
+function polygon(radius, numberOfSides) //drawing polygon with polar coordinates
+{
+    beginShape()
+
+	    for (var angle = 0; angle < TWO_PI; angle += TWO_PI/numberOfSides)
+	    {
+	    	var r = radius
+	    	var x = r * cos(angle)
+	    	var y = r * sin(angle)
+
+	    	vertex(x, y)
+	    }
+
+	endShape()
+
+	bottomHeight = r * sin(TWO_PI/numberOfSides) //calculate the height from center to bottom, which is shorter than radius
+	sideLength = r * cos(TWO_PI/numberOfSides) * 2
+}
+
+
+function fourHexgons(radius, numberOfSides)
+{
+	polygon(radius, numberOfSides)
+
+
+	push() //top right hexgon
+	    translate(1.5 * sideLength, 1 * bottomHeight)
+
+	    fill(1, 0.68, 0.29)
+
+	    polygon(radius, numberOfSides)
+	pop()
+
+	push() //bottom left hexgon
+	    translate(0 * sideLength, 2 * bottomHeight)
+
+	    fill(0.27, 0.65, 0.76)
+
+	    polygon(radius, numberOfSides)
+	pop()
+
+	push() //bottom right hexgon
+	    translate(1.5 * sideLength, 1 * bottomHeight)
+	    translate(0 * sideLength, 2 * bottomHeight)
+
+	    fill(0.01, 0.29, 0.48)
+
+	    polygon(radius, numberOfSides)
+	pop()
+}
+
+
+
+
+
