9d4ca68656756ce00fbd4449b87baeb315e48e64
heranzhou <hr.zhou9446@gmail>
2021-07-10 20:43:55 -0400

    dfghf

---
+++ CircleFill/CC_animated_circle_packing/sketch.js
@@ -0,0 +1,12 @@
+
+
+
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw() 
+{
+	
+}
--- CircleFill/Circle_Packing/sketch.js
+++ CircleFill/Circle_Packing/sketch.js
@@ -3,6 +3,7 @@ let circles = []
 let collide_num = 0
 let protection = 0
 
+let radiusMin = 5
 
 function setup() 
 {
@@ -10,18 +11,18 @@ function setup()
 	colorMode(RGB, 1)
 	noFill()
 
-	for (let i=0; i<10; i++)
-	{
-		circles.push(new Circle())
-	}
+	// for (let i=0; i<10; i++)
+	// {
+	// 	circles.push(new Circle())
+	// }
 
 
-	for (let i of circles)
-	{
-		i.show()
-		//print(i.x)
-		//print(i.y)
-	}
+	// for (let i of circles)
+	// {
+	// 	i.show()
+	// 	//print(i.x)
+	// 	//print(i.y)
+	// }
 
 
 
@@ -32,11 +33,43 @@ function draw()
 {
 	background(0.5)
 
-	circles.add(new Circle(random(width), random(height)))
+	//circles.push(new Circle(random(width), random(height)))
+	let circle = newCircle()
+
+	// if (circle !== null)
+	// {
+	// 	circles.push(circle)
+	// }
+
+
 
 
 	for (let i of circles)
 	{
+		if (i.growing) //if a circle is growing, 2 conditions to stop growing
+		{
+			if (i.onEdges()) i.growing = false
+
+			else
+			{
+				for (let j of circles)
+				{
+					if (i != j) //every other circle. not itself
+					{
+						let d = dist(i.x, i.y, j.x, j.y)
+
+						if (d < i.r + j.r)
+						{
+							i.growing = false
+
+							break
+						}
+					}	
+				}
+			}
+		}
+
+
 		i.show()
 		i.grow()
 	}
@@ -44,38 +77,70 @@ function draw()
     
   
 
+
+
+function newCircle()
+
+{
+	let x = random(width)
+	let y = random(height)
+
+	let valid = true
+
+	for (let i of circles)
+	{
+
+		let d = dist(x, y, i.x, i.y)
+
+		if (d < i.r + radiusMin)
+		{
+			valid = false
+			break
+		}
+	}
+
+
+	if (valid) circles.push(new Circle(x,y))
+
+	else return null
+
+}
+
+
+
+
+
 class Circle
 {
-	constructor()
+	constructor(x, y)
 	{
-		this.x = random(width)
-		this.y = random(height)
-		this.dia = 5
+		this.x = x
+		this.y = y
+		this.r = radiusMin
+
+		this.growing = true
 	}
 
 	show()
 	{
-		ellipse(this.x, this.y, this.dia, this.dia)
+		ellipse(this.x, this.y, this.r*2, this.r*2)
 	}
 
 	grow()
 	{
-		if (!this.onEdges())
+		if (this.growing)
 		{
-			//print("fsfsfs")
-			this.dia += 1
-			//print(this.dia)
+			this.r += 1
 		}
 	}
 
 	onEdges()
 	{
-		return (this.x-this.dia/2 < 0 || 
-			this.x+this.dia/2 > width || 
-			this.y-this.dia/2 < 0 || 
-			this.y+this.dia/2 > height)
+		return (this.x-this.r < 0	  || 
+				this.x+this.r > width || 
+				this.y-this.r < 0     ||  
+				this.y+this.r > height)
 	}
-
 
 
 
